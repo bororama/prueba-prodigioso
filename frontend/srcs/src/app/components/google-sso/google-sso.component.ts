58acf3befffb176ae9f974509661532d7e4ff9f7
Nicolas Pinto gomez <nicolas.pinto.gomez@gmail.com>
2023-03-01 12:54:58 +0100

    [APPLICATION] Requested functionality fulfilled

--- frontend/srcs/src/app/components/google-sso/google-sso.component.ts
+++ frontend/srcs/src/app/components/google-sso/google-sso.component.ts
@@ -17,19 +17,9 @@ export class GoogleSsoComponent implements OnInit {
     //e m p t y, but google likes it
   }
 
-  /*
-  **  The GET request first retrieves the app's client id.
-  **  Upon completion, the google.accounts.id object is initialized and
-  **  its login button is rendered inside the subscribe callback.
-  **  'ux_mode : redirect' makes it so that handleCredentialResponse() is never called.
-  **  Instead, a POST request to the endpoint specified in login_uri is made.
-  **  Said request contains the corresponding credentials and are thus validated
-  **  server side. After a successful validation of credentials, the server
-  **  redirects the client to localhost:9778/home
-  */
-
+  
   async ngOnInit() {
-
+    
     this.loadGoogle();
     
   }
@@ -40,23 +30,33 @@ export class GoogleSsoComponent implements OnInit {
     googleLibrary.onload =  () => {this.displayGoogleButton();}
     document.body.appendChild(googleLibrary);
   }
-
-    private displayGoogleButton() {
-      this.http.get<any>('/api/client_id', {observe: 'body'}).subscribe((value) => {
-        //@ts-ignore
-        google.accounts.id.initialize({
-        client_id:  value.client_id,
-        callback: this.handleCredentialResponse,  
-        ux_mode: "redirect",
-        login_uri: "http://localhost:3000/api/login"
-        })
-        //@ts-ignore
-        google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }
-        );
-      });
-    }
+  
+  /*
+  **  The GET request first retrieves the app's client id.
+  **  Upon completion, the google.accounts.id object is initialized and
+  **  its login button is rendered inside the subscribe callback.
+  **  'ux_mode : redirect' makes it so that handleCredentialResponse() is never called.
+  **  Instead, a POST request to the endpoint specified in login_uri is made.
+  **  Said request contains the corresponding credentials and are thus validated
+  **  server side. After a successful validation of credentials, the server
+  **  redirects the client to localhost:9778/home
+  */
+  private displayGoogleButton() {
+    this.http.get<any>('/api/client_id', {observe: 'body'}).subscribe((value) => {
+      //@ts-ignore
+      google.accounts.id.initialize({
+      client_id:  value.client_id,
+      callback: this.handleCredentialResponse,  
+      ux_mode: "redirect",
+      login_uri: "http://localhost:3000/api/login"
+      })
+      //@ts-ignore
+      google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { theme: "outline", size: "large" }
+      );
+    });
+  }
 }
   
 
